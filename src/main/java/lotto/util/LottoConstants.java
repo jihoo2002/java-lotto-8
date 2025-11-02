aaa18529ee7c5849fbf106a0c46ebc79712b5ee7
jihoo2002 <yolohoo@naver.com>
2025-11-03 03:00:13 +0900

    feat : 로또 관련 전역적으로 쓰일 상수들을 정의한 LottoConstants 추가

---
+++ src/main/java/lotto/util/LottoConstants.java
@@ -0,0 +1,8 @@
+package lotto.util;
+
+public class LottoConstants {
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_NUMBERS_PER_TICKET = 6;
+    public static final int LOTTO_TICKET_PRICE = 1000;
+}
