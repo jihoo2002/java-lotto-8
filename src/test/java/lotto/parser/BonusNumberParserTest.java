e1a0014a3965f44e7eeaf8ed5602ab0a509aff5f
jihoo2002 <yolohoo@naver.com>
2025-11-03 03:20:14 +0900

    refactor : BonusNumberParserTest에 누락된 display 추가

--- src/test/java/lotto/parser/BonusNumberParserTest.java
+++ src/test/java/lotto/parser/BonusNumberParserTest.java
@@ -1,6 +1,7 @@
 package lotto.parser;
 
 import lotto.util.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,6 +12,7 @@ class BonusNumberParserTest {
 
     @ParameterizedTest
     @NullAndEmptySource
+    @DisplayName("빈문자열 또는 null 입력 시 예외가 발생한다")
     void 빈문자열또는_null_예외발생(String input) {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
@@ -22,6 +24,7 @@ class BonusNumberParserTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"abc", "1a", "!"})
+    @DisplayName("숫자가 아닌 값을 입력 시 예외가 발생한다")
     void 숫자아닌_입력_예외발생(String input) {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
@@ -33,6 +36,7 @@ class BonusNumberParserTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"0", "46", "100"})
+    @DisplayName("입력값이 범위에 벗어나면 예외가 발생한다")
     void 범위벗어난_입력_예외발생(String input) {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
